62557bafe53e36940f9a9ee6d142de9628bf7d9f
Evgeny <beloysov_jenya@inbox.ru>
2023-01-20 23:08:38 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N: 2 -> "-2, -1, 0, 1, 2"
+
+//System.Console.WriteLine("Введите число "); // Рабочая версия через FOR
+//int N = Convert.ToInt32(Console.ReadLine());
+
+//for (int i = -N; i <= N; i++)
+    //{
+   //     System.Console.WriteLine(i);
+   // }
+
+
+
+//System.Console.Write("Введите число: "); // РАБОЧАЯ ВЕРСИЯ из лекции, не выводит последнее число
+//int N = Convert.ToInt32(Console.ReadLine());
+//int M = N * -1;
+
+//while (M != N)
+//{
+//    System.Console.WriteLine(M);
+  //  M += 1;
+//}
+
+System.Console.WriteLine("Введите число "); // МОЯ ВЕРСИЯ
+int N = Convert.ToInt32(Console.ReadLine());
+int i = -N;
+
+while(i <= N){
+    {
+        System.Console.WriteLine(i);
+        i = i + 1; 
+    }
+}
