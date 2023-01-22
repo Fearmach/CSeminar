d9e567ad0cd44d6e8fb206e2093cee600421d8c4
Evgeny <beloysov_jenya@inbox.ru>
2023-01-22 23:08:18 +0300

    add to task8 reminder

--- task8/Program.cs
+++ task8/Program.cs
@@ -12,3 +12,19 @@ else
 {
     System.Console.WriteLine($" число 1 не кратно числу 2, остаток от деления = {num1 % num2}");
 }
+
+// System.Console.WriteLine("Введите 1-е число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите 2-е число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int reminder = num1 % num2
+
+// if (reminder == 0)
+// {
+//     System.Console.WriteLine($"{num1} кратно {num2}");
+// }
+// else
+// {
+//     System.Console.WriteLine($" число 1 не кратно числу 2, остаток от деления = {reminder}");
+// }
