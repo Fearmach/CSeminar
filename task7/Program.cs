19cca7987d894de10eb46a6024bf55f74d51bf66
Evgeny <beloysov_jenya@inbox.ru>
2023-01-22 22:56:10 +0300

    edit task7

--- task7/Program.cs
+++ task7/Program.cs
@@ -4,7 +4,9 @@
 int randNumber = new Random().Next(100, 1000); 
 System.Console.WriteLine(randNumber);
 
-int a = (randNumber/10);
-int b = (randNumber%10);
+//int a = (randNumber/10);
+//int b = (randNumber%10);
 
-System.Console.WriteLine($"{a/10}{b}");
+//System.Console.WriteLine($"{a/10}{b}");
+
+System.Console.WriteLine((randNumber/100) * 10 + randNumber%10);
