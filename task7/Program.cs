b21e00315aa3f416ff390f29d4fdc8da8b439726
Evgeny <beloysov_jenya@inbox.ru>
2023-01-22 22:37:37 +0300

    add task7

---
+++ task7/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа (456 -> 46)
+
+
+int randNumber = new Random().Next(100, 1000); 
+System.Console.WriteLine(randNumber);
+
+int a = (randNumber/10);
+int b = (randNumber%10);
+
+System.Console.WriteLine($"{a/10}{b}");
