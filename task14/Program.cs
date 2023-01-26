d7976794f71b8a450498085af297b497f1168590
Evgeny <beloysov_jenya@inbox.ru>
2023-01-26 23:47:35 +0300

    edit task 14

--- task14/Program.cs
+++ task14/Program.cs
@@ -1,30 +1,29 @@
 ﻿// Написать программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N (5 -> 1, 4, 9, 16, 25; 2 -> 1, 4)
 
-// System.Console.WriteLine("Введите число: ");
-// double N = Convert.ToInt32(Console.ReadLine());
-
-// for (double i = 0; i < N; i++)
-// {
-//     System.Console.WriteLine($"{Math.Pow(N, 2)}");
-// }
-
 System.Console.WriteLine("Введите число: ");
-int N = Convert.ToInt32(Console.ReadLine());
-int count = 1;
-double answer = 0;
+double N = Convert.ToInt32(Console.ReadLine());
 
-if (N > 0)
-{
-    while (count <= N)
-    {
-        answer = Math.Pow(count,2);
-        System.Console.WriteLine(answer);
-        count ++;
-    }
-}
-else
-{
-    System.Console.WriteLine("Не корректное число");
-}
+for (double i = 1; i <= N; i++)
+
+System.Console.WriteLine($"{Math.Pow(i,2)}");
+
+// System.Console.WriteLine("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// double answer = 0;
+
+// if (N > 0)
+// {
+//     while (count <= N)
+//     {
+//         answer = Math.Pow(count,2);
+//         System.Console.WriteLine(answer);
+//         count ++;
+//     }
+// }
+// else
+// {
+//     System.Console.WriteLine("Не корректное число");
+// }
 
 
