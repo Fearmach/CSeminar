8f8decac0167d9cdd2d6113ee1eb2602cc5b6e65
Evgeny <beloysov_jenya@inbox.ru>
2023-01-27 19:38:16 +0300

    add task 17, 18

---
+++ task17/Program.cs
@@ -0,0 +1,25 @@
+﻿// Написать программу, которая принимает на вход число и выдает количество цифр в числе (456 - 3, 78 - 2)
+
+// Проверка на корректность введенного числа 1 Вариант
+
+ int InputNumber(string str)
+ {
+     int number;
+     string text;
+     while(true)
+     {
+         System.Console.WriteLine(str);
+         text = Console.ReadLine();
+         if (int.TryParse(text, out number))
+         {
+             break;
+         }
+         System.Console.WriteLine("Введено не корректное число, попробуйте еще раз!");
+     }
+     return number;
+}
+
+int num = InputNumber("Введите число: ");
+string numString = Convert.ToString(num);
+int length = numString.Length;
+System.Console.WriteLine(length);
---
+++ task18/Program.cs
@@ -0,0 +1,33 @@
+﻿// Написать программу, которая принимает на вход число N и выдает произведение чисел от 1 до N (4 - 24, 5 - 120)
+
+ int InputNumber(string str)
+ {
+     int number;
+     string text;
+     while(true)
+     {
+         System.Console.WriteLine(str);
+         text = Console.ReadLine();
+         
+         if (int.TryParse(text, out number))
+         {
+             break;
+         }
+         System.Console.WriteLine("Введено не корректное число, попробуйте еще раз!");
+     }
+     return number;
+}
+
+int N = InputNumber("Введите число: ");
+
+int Math_Method(int number)
+{
+    int res = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        res *= i;
+    }
+    return res;
+}
+int result = Math_Method(N);
+System.Console.WriteLine(result);
