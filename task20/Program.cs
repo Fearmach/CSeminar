2895b00654d63a7771f46445b0a0c96fb4d43481
Evgeny <beloysov_jenya@inbox.ru>
2023-01-29 23:14:03 +0300

    edit task20

--- task20/Program.cs
+++ task20/Program.cs
@@ -1,37 +1,38 @@
-﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка {-9 - 9}. Найдите сумму отрицательных и положительных элементов массива. в массиве (3, 9, -8, 1, 0, -7, 2, -1, 8, -3, -1, 6) сумма положительных = 29, отрицательных -20
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+// Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
 
 int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange) // СОЗДАЛИ ФУНКЦИЮ ЗАПОЛНЕНИЯ МАССИВА
 {
-    int[] arr = new int[size]; // СОЗДАЛИ МАССИВ И ВЫДЕЛИЛИ НА НЕГО ПАМЯТЬ
-    Random rand = new Random(); // МЕТОД ЗАПОЛНЕНИЯ
-    for (int i = 0; i < arr.Length; i++)
-    {
-        arr[i] = rand.Next(leftRange, rightRange + 1);
-    }
-    return arr;
+int[] arr = new int[size]; // СОЗДАЛИ МАССИВ И ВЫДЕЛИЛИ НА НЕГО ПАМЯТЬ
+Random rand = new Random(); // МЕТОД ЗАПОЛНЕНИЯ
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rand.Next(leftRange, rightRange + 1);
+}
+return arr;
 }
 
-void SumPositiveAndNegativeElements(int[] arr, out int sumP, out int sumN);
-
+void SumPositiveAndNegativeElements(int[] arr, out int sumP, out int sumN)
 {
-    sumP = 0;
-    sumN = 0;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (arr[i] > 0)
-        {
-            sumP += arr[i];
-        }
-        else
-        {
-            sumN += arr[i];
-        }
-    }
+sumP = 0;
+sumN = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+if (arr[i] > 0)
+{
+sumP += arr[i];
+}
+else
+{
+sumN += arr[i]; 
+}
+}
 }
 
 void PrintArray(int[] arr) // ФУНКЦИЯ ВЫВОДА
 {
-    System.Console.WriteLine("[" + string.Join(", ", arr) + "]"); // string.Join Позволяет вывести все элементы массива например через (,  )
+System.Console.WriteLine("[" + string.Join(", ", arr) + "]"); // string.Join Позволяет вывести все элементы массива например через (,  )
 }
 
 int[] array = FillArrayWithRandomNumbers(6, -9, 9); // СОЗДАЛИ МАССИВ И ВЫДЕЛИЛИ НА НЕГО ПАМЯТЬ
