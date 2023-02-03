dfaaaa58d68a54730a0e62f516eb236658d13f33
Evgeny <beloysov_jenya@inbox.ru>
2023-02-03 22:39:23 +0300

    edit task 27

---
+++ task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу, которая будет преобразовывать десятичное число в двоичное (45 -> 101101; 3 -> 11)
+
+
+void GetBinaryView(int numb)
+{
+    if (numb <= 0) return;
+    GetBinaryView(numb / 2);
+    System.Console.Write(numb % 2);
+}
+
+int Numb (string text)
+{
+    System.Console.WriteLine(text);
+    int num = Convert.ToInt32(Convert.ToString(System.Console.ReadLine()));
+    return num;
+}
+
+int N = Numb("Введите число: ");
+GetBinaryView(N);
