160b1c50e2cf9b438a2d624c18408526b5c7c6b4
Evgeny <beloysov_jenya@inbox.ru>
2023-02-13 13:34:03 +0300

    add task 34

---
+++ task34/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте двумерный массив, напишите программу, которая поменяет местами первую и последнюю строчки массива.
+
+int[,] FillMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matr = new int[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matr[i, j] = rand.Next(0, 50);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void ReversMatrix(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        temp = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = temp;
+    }
+}
+
+int[,] matrix = FillMatrix(3, 4);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+ReversMatrix(matrix);
+PrintMatrix(matrix);
