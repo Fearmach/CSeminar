c3f05c9ac0a883c346c14872831b28ada5a4ce9b
Evgeny <beloysov_jenya@inbox.ru>
2023-02-13 14:38:06 +0300

    add task 36

---
+++ task36/Program.cs
@@ -0,0 +1,50 @@
+﻿// Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных
+
+int[,] FillMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matr = new int[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matr[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] matr = FillMatrix(3, 4);
+PrintMatrix(matr);
+int[] result = new int[10];
+
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        result[matr[i, j]]++;
+    }
+    System.Console.WriteLine();
+}
+System.Console.WriteLine(string.Join(" ", result));
+
+for (int i = 0; i < result.Length; i++)
+{
+    if (result[i] != 0)
+    {
+        System.Console.WriteLine($"{i} встречается {result[i]} раз");
+    }
+}
